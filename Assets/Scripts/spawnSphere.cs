98f835e2e96338b8986db938e940c2243f23eb08
Matyasmester <matyas.zoboki@gmail.com>
2019-05-14 19:51:45 +0200

    First exercise. A sphere falling inside a container.

---
+++ Assets/Scripts/spawnSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spawnSphere : MonoBehaviour {
+
+    public Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("space"))
+        {
+            //Debug.Log("Az input mukszik.");
+            FindObjectOfType<MeshRenderer>().enabled = true;
+            FindObjectOfType<Transform>().position = pos;
+            
+        }
+    }
+}
